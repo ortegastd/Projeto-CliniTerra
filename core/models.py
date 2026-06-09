ffc01e305e85775a73c178e4900b837501d6fe82
ortegastd <ortegamd79@gmail.com>
2026-06-09 14:52:40 -0300

    Primeiro commit - ProjetoCliniTerra funcionando

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    idade = models.IntegerField()
+    # ADICIONE ESTA LINHA ABAIXO:
+    telefone = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
--- projeto_terra/urls.py
+++ projeto_terra/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from core.views import home_view, cadastro_view, lista_view, limpar_banco_view, excluir_usuario_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', home_view, name='home'),                                             # Tela de Boas-Vindas
+    path('cadastro/', cadastro_view, name='cadastro'),                           # Tela de Cadastro
+    path('clientes/', lista_view, name='lista_clientes'),                        # Banco de Dados Cadastrais
+    path('limpar/', limpar_banco_view, name='limpar_banco'),                     # Apagar tudo
+    path('excluir/<int:id_usuario>/', excluir_usuario_view, name='excluir_usuario'), # Lixeira individual
 ]
